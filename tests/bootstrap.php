314f2f14549cc1fde84aae87298e81332bc4f285
Carlos C Soto <eclipxe13@gmail.com>
2024-10-03 08:05:13 -0600

    Load `tests/.env` when it exists

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,6 +9,8 @@ error_reporting(-1);
 require_once __DIR__ . '/../vendor/autoload.php';
 
 (function (): void {
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-    $dotenv->load();
+    if (file_exists(__DIR__ . '/.env')) {
+        $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+        $dotenv->load();
+    }
 })();
