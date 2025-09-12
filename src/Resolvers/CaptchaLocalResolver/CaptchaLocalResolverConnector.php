e924e0c65e8466fdb386fcd9fefc03b0ea040546
Carlos C Soto <eclipxe13@gmail.com>
2025-09-12 11:08:18 -0600

    Better check for converting code & answer values

--- src/Resolvers/CaptchaLocalResolver/CaptchaLocalResolverConnector.php
+++ src/Resolvers/CaptchaLocalResolver/CaptchaLocalResolverConnector.php
@@ -86,7 +86,7 @@ class CaptchaLocalResolverConnector
         }
         $contents = strval($response->getBody());
         $data = json_decode($contents, true);
-        $code = (is_array($data)) ? strval($data['code'] ?? '') : '';
+        $code = (is_array($data) && isset($data['code']) && is_scalar($data['code'])) ? strval($data['code']) : '';
         if ('' === $code) {
             throw new RuntimeException('Image was sent but service returns empty code');
         }
@@ -94,11 +94,11 @@ class CaptchaLocalResolverConnector
     }
 
     /**
-     * Check code for answer, if empty string means answer does not exists yet
+     * Check code for answer, if empty string means answer does not exist yet
      *
      * @param string $code
      * @return string
-     * @throws RuntimeException if code does not exists
+     * @throws RuntimeException if code does not exist
      * @throws RuntimeException if unable to check code
      * @throws RuntimeException if http transaction error occurs
      */
@@ -119,7 +119,7 @@ class CaptchaLocalResolverConnector
         }
         $contents = strval($response->getBody());
         $data = json_decode($contents, true);
-        return (is_array($data)) ? strval($data['answer'] ?? '') : '';
+        return (is_array($data) && isset($data['answer']) && is_scalar($data['answer'])) ? strval($data['answer']) : '';
     }
 
     public function buildUri(string $action): string
